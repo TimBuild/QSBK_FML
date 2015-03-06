4d4bc0cb6df6e6ca473d772b0772d5a13189748f
brad <brad@gmail.com>
2015-03-06 11:46:57 +0800

    aa

---
+++ Client/QiuBai/src/com/qiubai/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qiubai.activity;
+
+import com.tb.qiubai.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
