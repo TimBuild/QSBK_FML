7eeb6d3589abcc9d891c67844461b76b481217c2
brad <symworkd@163.com>
2015-03-12 17:21:00 +0800

    aaaa

--- QiuBai/src/com/bt/qiubai/DetailActivity.java
+++ QiuBai/src/com/bt/qiubai/DetailActivity.java
@@ -2,16 +2,28 @@ package com.bt.qiubai;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class DetailActivity extends Activity {
 	
-	private RelativeLayout title_rel_left,title_rel_right;
+	private RelativeLayout title_back,title_rel_right;
 	private Dialog actionDialog;
+	
+	private GestureDetector gestureDetector;
+	
+	private LinearLayout action_share,action_comment;
+	private ScrollView scroll_content;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +33,87 @@ public class DetailActivity extends Activity {
 		setContentView(R.layout.activity_detail);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.detail_title);
 		
-		title_rel_left = (RelativeLayout) findViewById(R.id.title_rel_left);
-		title_rel_right = (RelativeLayout) findViewById(R.id.title_rel_right);
+		gestureDetector = new GestureDetector(DetailActivity.this,onGestureListener);
 		
-		title_rel_left.setOnClickListener(new OnClickListener() {
+		scroll_content = (ScrollView) findViewById(R.id.detail_scroll_content);
+		scroll_content.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return gestureDetector.onTouchEvent(event);
+			}
+		});
+		
+		actionDialog = new Dialog(DetailActivity.this, R.style.DetailActionDialog);
+		actionDialog.setContentView(R.layout.detail_action_bar);
+		actionDialog.getWindow().setGravity(Gravity.RIGHT | Gravity.TOP);
+		
+		action_share = (LinearLayout) actionDialog.findViewById(R.id.detail_action_share);
+		action_comment = (LinearLayout) actionDialog.findViewById(R.id.detail_action_comment);
+				
+		
+		
+		action_share.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
+			}
+		});
+		action_comment.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+		
+		title_back = (RelativeLayout) findViewById(R.id.title_rel_left_back);
+		title_rel_right = (RelativeLayout) findViewById(R.id.title_rel_right);
+		
+		title_back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
 			}
 		});
 		
 		title_rel_right.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				
+				actionDialog.show();
 			}
 		});
 		
-		//actionDialog = new Dialog(DetailActivity.this, R.style.MyConfirmDialog);
-		//confirmDialog.setContentView(R.layout.confirm_dialog);
 		
 	}
+	
+	private GestureDetector.OnGestureListener onGestureListener = new GestureDetector.SimpleOnGestureListener() {
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			float x = Math.abs(e2.getX() - e1.getX());
+			float y = Math.abs(e2.getY() - e1.getY());
+			if(y < x){
+				if(e2.getX() - e1.getX() > 200){
+					//Intent intent = new Intent(NoteActivity.this, WeatherActivity.class);
+					//startActivity(intent);
+					//activity屏幕切换动画----左右滑动切换
+					//overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
+					System.out.println("right");
+					finish();
+					return true;
+				}else if(e2.getX() - e1.getX() < -200){
+					Intent intent = new Intent(DetailActivity.this, CommentActivity.class);
+					startActivity(intent);
+					//overridePendingTransition(R.anim.in_from_right, R.anim.out_to_left);
+					return true;
+				}
+			}else {
+				return false;
+			}
+			
+			return false;
+		}
+	};
 }
