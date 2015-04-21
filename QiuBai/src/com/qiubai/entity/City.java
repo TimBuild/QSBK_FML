91cc738536f3c1211cf125dd6e8cd9175f0c7054
Tim <shang6466575@163.com>
2015-04-21 14:48:57 +0800

    添加选择城市

---
+++ QiuBai/src/com/qiubai/entity/City.java
@@ -0,0 +1,29 @@
+package com.qiubai.entity;
+
+public class City {
+	/**
+	 * 省份(江苏)
+	 */
+	private String province;
+	/**
+	 * 相对应的城市(镇江)
+	 */
+	private String town;
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	public void setTown(String town) {
+		this.town = town;
+	}
+
+}
