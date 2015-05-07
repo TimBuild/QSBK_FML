bd2da2b3ac274c7f38ffcb1149bb84cb7b1d7690
brad <symworkd@163.com>
2015-05-07 17:49:36 +0800

    ImageUtil

--- QiuBai/src/com/qiubai/util/ImageUtil.java
+++ QiuBai/src/com/qiubai/util/ImageUtil.java
@@ -1,11 +1,17 @@
 package com.qiubai.util;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.UUID;
 
 import org.apache.http.HttpStatus;
 
@@ -56,91 +62,63 @@ public class ImageUtil {
 		}
 		return null;
 	}
-	    /**
-	     * 上传文件到服务器
-	     * @param file 需要上传的文件
-	     * @param RequestURL 请求的rul
-	     * @return 返回响应的内容
-	     */
-	    public static int uploadFile(File file, String RequestURL) {
-	    	 private static final String TAG = "uploadFile";
-	 	    private static final int TIME_OUT = 10 * 1000; // 超时时间
-	 	    private static final String CHARSET = "utf-8"; // 设置编码
-	        int res=0;
-	        String result = null;
-	        String BOUNDARY = UUID.randomUUID().toString(); // 边界标识 随机生成
-	        String PREFIX = "--", LINE_END = "\r\n";
-	        String CONTENT_TYPE = "multipart/form-data"; // 内容类型
-	 
-	        try {
-	            URL url = new URL(RequestURL);
-	            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	            conn.setReadTimeout(TIME_OUT);
-	            conn.setConnectTimeout(TIME_OUT);
-	            conn.setDoInput(true); // 允许输入流
-	            conn.setDoOutput(true); // 允许输出流
-	            conn.setUseCaches(false); // 不允许使用缓存
-	            conn.setRequestMethod("POST"); // 请求方式
-	            conn.setRequestProperty("Charset", CHARSET); // 设置编码
-	            conn.setRequestProperty("connection", "keep-alive");
-	            conn.setRequestProperty("Content-Type", CONTENT_TYPE + ";boundary="+ BOUNDARY);
-	 
-	            if (file != null) {
-	                /**
-	                 * 当文件不为空时执行上传
-	                 */
-	                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-	                StringBuffer sb = new StringBuffer();
-	                sb.append(PREFIX);
-	                sb.append(BOUNDARY);
-	                sb.append(LINE_END);
-	                /**
-	                 * 这里重点注意： name里面的值为服务器端需要key 只有这个key 才可以得到对应的文件
-	                 * filename是文件的名字，包含后缀名
-	                 */
-	 
-	                sb.append("Content-Disposition: form-data; name=\"file\"; filename=\""
-	                        + file.getName() + "\"" + LINE_END);
-	                sb.append("Content-Type: application/octet-stream; charset="
-	                        + CHARSET + LINE_END);
-	                sb.append(LINE_END);
-	                dos.write(sb.toString().getBytes());
-	                InputStream is = new FileInputStream(file);
-	                byte[] bytes = new byte[1024];
-	                int len = 0;
-	                while ((len = is.read(bytes)) != -1) {
-	                    dos.write(bytes, 0, len);
-	                }
-	                is.close();
-	                dos.write(LINE_END.getBytes());
-	                byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINE_END)
-	                        .getBytes();
-	                dos.write(end_data);
-	                dos.flush();
-	                /**
-	                 * 获取响应码 200=成功 当响应成功，获取响应的流
-	                 */
-	                 res = conn.getResponseCode();
-	                Log.e(TAG, "response code:" + res);
-	                if (res == 200) {
-	                    Log.e(TAG, "request success");
-	                    InputStream input = conn.getInputStream();
-	                    StringBuffer sb1 = new StringBuffer();
-	                    int ss;
-	                    while ((ss = input.read()) != -1) {
-	                        sb1.append((char) ss);
-	                    }
-	                    result = sb1.toString();
-	                    Log.e(TAG, "result : " + result);
-	                } else {
-	                    Log.e(TAG, "request error");
-	                }
+	
+	public static String uploadFile(File file, String uri){
+		String result = null;
+		String boundary = UUID.randomUUID().toString();
+		String content_type = "multipart/form-data";
+		String prefix = "--", line_end = "\r\n";
+		
+		try {
+			URL url = new URL(uri);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setReadTimeout(30000);
+			conn.setConnectTimeout(30000);
+			conn.setDoInput(true); // 允许输入流
+			conn.setDoOutput(true); // 允许输出流
+			conn.setUseCaches(false); // 不允许使用缓存
+			conn.setRequestMethod("POST"); // 请求方式
+			conn.setRequestProperty("Charset", "utf-8"); // 设置编码
+			conn.setRequestProperty("connection", "keep-alive");
+			conn.setRequestProperty("Content-Type", content_type + ";boundary="+ boundary);
+			if(file != null){
+				DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+				StringBuffer sb = new StringBuffer();
+				sb.append(prefix);
+				sb.append(boundary);
+				sb.append(line_end);
+				sb.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"" + line_end);
+	            sb.append("Content-Type: application/octet-stream; charset=utf-8" + line_end);
+	            sb.append(line_end);
+	            dos.write(sb.toString().getBytes());
+	            FileInputStream fis = new FileInputStream(file);
+	            byte[] bytes = new byte[1024];
+	            int len = 0;
+	            while ((len = fis.read(bytes)) != -1) {
+	                dos.write(bytes, 0, len);
 	            }
-	        } catch (MalformedURLException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        return res;
-	    }
+	            fis.close();
+	            dos.write(line_end.getBytes());
+	            byte[] end_data = (prefix + boundary + prefix + line_end).getBytes();
+	            dos.write(end_data);
+	            dos.flush();
+	           
+	            if (conn.getResponseCode() == HttpStatus.SC_OK) {
+	                InputStream input = conn.getInputStream();
+	                StringBuffer sb1 = new StringBuffer();
+	                int ss;
+	                while ((ss = input.read()) != -1) {
+	                    sb1.append((char) ss);
+	                }
+	                result = sb1.toString();
+	            } else {
+	            }
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	       
 }
